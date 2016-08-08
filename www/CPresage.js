578da0ea2a3f7f1c54b7e2d90bef1ffecff111e0
jaanguita <jose.anguita@marfeel.com>
2016-08-08 10:21:13 +0200

    Added RECEIVE_BOOT_COMPLETED permission and js interface as a module export.

--- www/CPresage.js
+++ www/CPresage.js
@@ -1,5 +1,10 @@
+
+var exec = require('cordova/exec');
+
 var CPresage = {
 	adToServe: function(onAdEvent, onAdNotFound) {
 		cordova.exec(onAdEvent, onAdNotFound, 'CPresage', 'adToServe', [{}]);
 	}
 };
+
+module.exports = CPresage;
